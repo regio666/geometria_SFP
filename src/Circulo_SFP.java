692b78a00889ead3ab561a93cf1543f58da1b1d7
Sergio <kali.sfp@gmail.com>
2018-05-02 22:19:15 +0200

    Versión refactorizada

--- src/Circulo_SFP.java
+++ src/Circulo_SFP.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_SFP extends FiguraGeometrica_SFP {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_SFP(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
